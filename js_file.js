2ef09381e65ed846906f4b8bf3e3edccb9f46b22
Alex Kurcan <akurcan@my.waketech.edu>
2025-01-29 13:25:01 -0500

    adding code

---
+++ js_file.js
@@ -0,0 +1,24 @@
+const ageInput = document.getElementById('ageInput');
+const submitBtn = document.getElementById('submitButton');
+const result = document.getElementById('result');
+
+let age;
+
+submitBtn.onclick = function() {
+    age = ageInput.value;
+    age = Number(age); // change to integer
+
+    if (age >= 18 && age <= 100) {
+        result.textContent = "Perfect age!";
+    } else if (age < 18 && age >= 13) {
+        result.textContent = "You are a teenager...NOT eligiable for this site";
+    } else if (age < 13 && age >= 0) {
+        result.textContent = "You are a child...get off this site";
+    } else if (age < 0) {
+        result.textContent = "Your age isn't possible";
+    } else if (age == 0) {
+        result.textContent = "How were you just born?...";
+    } else {
+        result.textContent = "That's not a valid input";
+    }
+};
